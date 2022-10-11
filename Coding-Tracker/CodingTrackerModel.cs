84b7053b2977b94ca0589fd92e99573fcd21bb75
Clyde <noneclyde@gmail.com>
2022-10-11 14:03:16 +0300

    Add Model

---
+++ Coding-Tracker/CodingTrackerModel.cs
@@ -0,0 +1,8 @@
+﻿
+internal class CodingTrackerModel
+{
+    internal int Id { get; set; }
+    internal DateTime StartTime { get; set; }
+    internal DateTime EndTime { get; set; }
+    internal int Duration { get; set; }
+}
