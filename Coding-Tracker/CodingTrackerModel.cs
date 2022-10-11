c9a1afa63fb4e561aa46480f5e76f8d265251029
Clyde <noneclyde@gmail.com>
2022-10-11 15:06:05 +0300

    Change types of fields

--- Coding-Tracker/CodingTrackerModel.cs
+++ Coding-Tracker/CodingTrackerModel.cs
@@ -1,8 +1,8 @@
 ﻿
 internal class CodingTrackerModel
 {
-    internal int Id { get; set; }
+    internal string? Id { get; set; }
     internal string? StartTime { get; set; }
     internal string? EndTime { get; set; }
-    internal int Duration { get; set; }
+    internal string? Duration { get; set; }
 }
