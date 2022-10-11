dc212e289eb5f216138b673024d57aad09249257
Clyde <noneclyde@gmail.com>
2022-10-11 19:06:34 +0300

    Add two methods for navigation

--- Coding-Tracker/CodingController.cs
+++ Coding-Tracker/CodingController.cs
@@ -1,6 +1,34 @@
 ﻿
 internal class CodingController
 {
+    private readonly Database Database = new();
 
+    internal void ShowMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("\nMain Menu");
+        Console.WriteLine("\nWhat would you like to do:");
+        Console.WriteLine("\nType 0 To Close Application");
+        Console.WriteLine("Type 1 To View All Records");
+        Console.WriteLine("Type 2 To Insert Record");
+        Console.WriteLine("Type 3 To Delete Record");
+        Console.WriteLine("Type 4 To Update Record");
+    }
+
+    internal void Navigate(int Number)
+    {
+        switch (Number)
+        {
+            case 0:
+                Environment.Exit(0);
+                break;
+            case 1:
+                Console.Clear();
+                break;
+            default:
+                Console.WriteLine("Wrong Input!");
+                break;
+        }
+    }
 }
 
