13674bd266544a89e08fd152ea5803013bbf4aad
Clyde <noneclyde@gmail.com>
2022-10-12 13:50:47 +0300

    Fix bug with Sql Command

--- Coding-Tracker/Database.cs
+++ Coding-Tracker/Database.cs
@@ -66,7 +66,7 @@ internal class Database
 
                 command.CommandText =
                     $@"INSERT INTO CodingTracker (StartTime, EndTime, Duration)
-                        VALUES ({StartTime}, {EndTime}, {Duration})";
+                        VALUES ('{StartTime}', '{EndTime}', {Duration})";
 
                 command.ExecuteNonQuery();
             }
