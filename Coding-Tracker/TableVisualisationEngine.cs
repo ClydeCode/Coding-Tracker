bd464fc9a7bbe57a3e5ba501f3a1ffbd94557651
Clyde <noneclyde@gmail.com>
2022-10-12 11:49:39 +0300

    Add TableVisualisationEngine class

---
+++ Coding-Tracker/TableVisualisationEngine.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleTableExt;
+
+internal class TableVisualisationEngine
+{
+    private readonly List<List<object>> TableData = new();
+
+    internal void Print()
+    {
+        ConsoleTableBuilder
+            .From(TableData)
+            .WithTitle("Coding Tracker", ConsoleColor.Blue, ConsoleColor.Black)
+            .WithColumn("ID", "Start Session", "End Session", "Duration")
+            .ExportAndWriteLine();
+    }
+
+    internal void Insert(List<CodingTrackerModel> list)
+    {
+        foreach (CodingTrackerModel model in list)
+        {
+            TableData.Add(
+                new List<object>
+                {
+                    model.Id,
+                    model.StartTime,
+                    model.EndTime,
+                    model.Duration
+                }
+                );
+        }
+    }
+}
