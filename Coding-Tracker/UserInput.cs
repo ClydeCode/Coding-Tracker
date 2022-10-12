a7c7a7cdbeca9775adf39e69fa4f7655ec4e1f00
Clyde <noneclyde@gmail.com>
2022-10-12 13:54:35 +0300

    Add UserInput class

--- Coding-Tracker/UserInput.cs
+++ Coding-Tracker/UserInput.cs
@@ -1,5 +1,19 @@
 ﻿
 internal class UserInput
 {
+    Validation Validation = new();
+    internal DateTime GetDate()
+    {
+        Console.WriteLine("Type Date <YYYY-MM-DD>:");
 
+        string? Date = Console.ReadLine();
+
+        while (!Validation.CheckDate(Date))
+        {
+            Console.WriteLine("Wrong Date format!");
+            Date = Console.ReadLine();
+        }
+
+        return DateTime.Parse(Date);
+    }
 }
